5c8e06f1fe290c61e9c51b62a8eef17e33ddf4fa
DERRICK ZZIWA <derricktab44@gmail.com>
2022-08-10 11:58:28 +0300

    d

--- admin/admissions.php
+++ admin/admissions.php
@@ -35,22 +35,18 @@ if (isset($_POST["approved"])) {
     // Check if image file is a actual image or fake image
     $check = getimagesize($_FILES["student_image"]["tmp_name"]);
     if ($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
       $uploadOk = 1;
     } else {
-      echo "File is not an image.";
       $uploadOk = 0;
     }
 
     // Check if file already exists
     if (file_exists($target_file)) {
-      echo "Sorry, file already exists.";
       $uploadOk = 0;
     }
 
     // Check file size
     if ($_FILES["student_image"]["size"] > 500000) {
-      echo "Sorry, your file is too large.";
       $uploadOk = 0;
     }
 
@@ -59,19 +55,17 @@ if (isset($_POST["approved"])) {
       $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
       && $imageFileType != "gif"
     ) {
-      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
       $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-      echo "Sorry, your file was not uploaded.";
+      // echo "Sorry, your file was not uploaded.";
       // if everything is ok, try to upload file
     } else {
       if (move_uploaded_file($_FILES["student_image"]["tmp_name"], $target_file)) {
-        echo "The file " . htmlspecialchars(basename($_FILES["student_image"]["name"])) . " has been uploaded.";
       } else {
-        echo "Sorry, there was an error uploading your file.";
+        mysqli_error($con);
       }
     } // ! FINISHED FILE UPPLOAD ! //
 
